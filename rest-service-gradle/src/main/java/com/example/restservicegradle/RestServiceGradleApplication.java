be2793ceacdf4108577f3f56b20809f1d2c61296
Albert Snow <ajsnow2012@gmail.com>
2023-08-09 01:47:31 -0500

    adding restful gradle build

---
+++ rest-service-gradle/src/main/java/com/example/restservicegradle/RestServiceGradleApplication.java
@@ -0,0 +1,13 @@
+package com.example.restservicegradle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestServiceGradleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestServiceGradleApplication.class, args);
+	}
+
+}
